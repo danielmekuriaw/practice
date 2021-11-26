68f11996023de73faba47efde6c8d7f34d727dd8
danielmekuriaw <69174537+danielmekuriaw@users.noreply.github.com>
2021-11-26 11:03:20 -0500

    Time: 3 ms (10.10%), Space: 37.8 MB (23.52%) - LeetHub

---
+++ the-kth-factor-of-n/the-kth-factor-of-n.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int kthFactor(int n, int k) {
+        List<Integer> factors = new ArrayList<>();
+            
+        for(int i = 1; i <= n; i++){
+            if (n % i == 0){
+                factors.add(i);        
+            }
+        }
+        
+        if (k > factors.size()){
+            return -1;
+            
+        } else {
+            Collections.sort(factors);
+            return factors.get(k-1);
+        }
+    }
+}
