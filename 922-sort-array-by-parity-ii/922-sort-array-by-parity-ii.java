615a9083d924a4c693aeee25671acc9f56262349
danielmekuriaw <69174537+danielmekuriaw@users.noreply.github.com>
2022-01-24 13:45:09 -0500

    Time: 1239 ms (5.08%), Space: 90.3 MB (5.31%) - LeetHub

---
+++ 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] sortArrayByParityII(int[] nums) {
+        List even_list = new ArrayList<Integer>();
+        List odd_list = new ArrayList<Integer>();
+        
+        int even_index = 0;
+        int odd_index = 0;
+        
+        int[] returned_array = new int[nums.length];
+        
+        for(int i = 0; i < nums.length; i++){
+            System.out.println("Even Index: " + even_index);
+            System.out.println("Odd Index: " + odd_index);
+            
+            System.out.println("I: " + i);
+            System.out.println("i%2: " + i%2);
+            
+            if(i%2 == 0){
+                while(nums[even_index]%2 != 0){
+                    even_index++;
+                }
+                
+                returned_array[i] = nums[even_index];
+                even_index++;
+                
+            } else {
+                while(nums[odd_index]%2 == 0){
+                    odd_index++;
+                }
+                
+                returned_array[i] = nums[odd_index];
+                odd_index++;
+            }
+        }
+        
+        return returned_array;
+
+    }
+}
