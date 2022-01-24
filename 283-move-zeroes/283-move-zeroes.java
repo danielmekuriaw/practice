7b6a70e1927fa7cd7c866eab550b43904e9b9b8f
danielmekuriaw <69174537+danielmekuriaw@users.noreply.github.com>
2022-01-23 22:44:17 -0500

    Time: 417 ms (5.02%), Space: 52.7 MB (6.41%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] nums) {       
+        boolean swap = true;
+        
+        while(swap){
+            swap = false;
+            
+            for(int i = 0; i < nums.length-1; i++){
+                if(nums[i] == 0 && nums[i+1] != 0){
+                    swap = true;
+                    
+                    int temp = nums[i];
+                    nums[i] = nums[i+1];
+                    nums[i+1] = temp;
+                }
+            }
+        }
+    }
+}
