5b55e7a9375bfc1cc375f8d9e89720fa08df43fd
danielmekuriaw <69174537+danielmekuriaw@users.noreply.github.com>
2022-01-26 23:17:17 -0500

    Time: 2 ms (54.64%), Space: 44.7 MB (5.53%) - LeetHub

---
+++ 243-shortest-word-distance/243-shortest-word-distance.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int shortestDistance(String[] wordsDict, String word1, String word2) {
+        boolean counter_switch = false;
+        int distance = 0;
+        int min_distance = 2147483647;
+        boolean second_round = false;
+        String not_word = "";
+        
+        for(int i = 0; i < wordsDict.length; i++){
+            if(wordsDict[i].equals(word1) || wordsDict[i].equals(word2)){
+                if(counter_switch){
+                    if(!second_round){
+                        
+                        if(!wordsDict[i].equals(not_word)){
+                            
+                            if(distance < min_distance){
+                                min_distance = distance;
+                            }
+                            
+                            distance = 1;
+                            not_word = wordsDict[i];
+                            second_round = false;
+                        }
+
+    
+                    } else {
+                        
+                        if(!wordsDict[i].equals(not_word)){
+                            
+                            if(distance < min_distance){
+                                min_distance = distance;
+                            }
+                            
+                            distance = 1;
+                            not_word = wordsDict[i];
+                            second_round = false;
+                        }
+                        
+                    }
+                    
+                } else {
+                    distance++;
+                    counter_switch = true;
+                }
+            } else if(counter_switch){
+                distance++;   
+            }
+            
+        }
+        
+        return min_distance;
+        
+    }
+}
