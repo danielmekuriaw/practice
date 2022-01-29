2e067457968acc33cfbfaaeb3c1b7126843b2649
danielmekuriaw <69174537+danielmekuriaw@users.noreply.github.com>
2022-01-29 18:25:36 -0500

    Time: 1 ms (95.41%), Space: 40.5 MB (6.40%) - LeetHub

---
+++ 362-design-hit-counter/362-design-hit-counter.java
@@ -0,0 +1,24 @@
+class HitCounter {
+    Set<Integer> hit_counts = new HashSet<Integer>();
+    List hits = new ArrayList<Integer>();
+
+    public HitCounter() {
+        
+    }
+    
+    public void hit(int timestamp) {
+        hits.add(timestamp);
+    }
+    
+    public int getHits(int timestamp) {
+        int count = 0;
+        for(int i = 0; i < hits.size(); i++){
+            int hit = (int) hits.get(i);
+            if(hit > timestamp - 300 && hit <= timestamp){
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
